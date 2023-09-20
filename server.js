ed41a03a5b0ba913a3d4c970fc4c88d3e39071da
yudong-yeob <maintain98@naver.com>
2023-09-20 21:02:58 +0900

    /message url 매핑 + 파일 생성

--- server.js
+++ server.js
@@ -2,17 +2,35 @@
  * http 경로를 가져옴
  * */
 const http = require('http');
-
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
 
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html lang="eng">');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></h1></body>');
+        res.write('</html>');
+        return res.end();
+
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html lang="eng">');
     res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('<body><h1>Hello Node.js</h1></body>');
     res.write('</html>');
-    res.end();
+    return res.end();
+
 
     /**
      * createServer 종료

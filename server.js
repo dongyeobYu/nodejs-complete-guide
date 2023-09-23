2e722aaa5403799618ab0485fbf96e894d7b073f
yudong-yeob <maintain98@naver.com>
2023-09-23 18:34:57 +0900

    req.on() 메서드 공부 data, end 의 처리 방법 및 동작 원리, 응용

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    //console.log(req.url, req.method, req.headers);
 
     const url = req.url;
     const method = req.method;
@@ -13,13 +13,29 @@ const server = http.createServer((req, res) => {
     if (url === '/') {
         res.write('<html lang="eng">');
         res.write('<head><title>My First Page</title></head>');
-        res.write('<body><h1><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></h1></body>');
+        res.write('<body><h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></h1></body>');
         res.write('</html>');
         return res.end();
 
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+
+        const body = [];
+        // 특정 이벤트를 들을 수 있다. ( ex. close, data, end, error, readable )
+        // data 를 읽는다 주의점) data 를 다 읽고 다음 데이터를 읽음 -> 다 읽었으면 "end" 메소드를 실행
+        // "data" 메소드를 사용하여 POST 데이터가 들어오면 chunk 를 사용하여 body 에 저장함
+        req.on("data", (chunk) =>{
+            console.log(chunk);
+            body.push(chunk);
+        } )
+        req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.appendFileSync("message.txt", message);
+
+            console.log(parsedBody);
+        })
+
         res.statusCode = 302;
         res.setHeader('Location', '/')
         return res.end();
